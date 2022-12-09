177964e5d0f3a3ed186f3a523461200e7294b22a
이창준 <crystalashe@naver.com>
2022-12-09 19:37:21 +0900

    [Chore] TaskService 생성

---
+++ DidYouFeed/DidYouFeed/Utils/Services/TaskService.swift
@@ -0,0 +1,23 @@
+//
+//  TaskService.swift
+//  DidYouFeed
+//
+//  Created by 이창준 on 2022/12/09.
+//
+
+import Foundation
+
+final class TaskService {
+    
+    // MARK: - Singleton
+    
+    static let shared = TaskService()
+    
+    // MARK: - Initializer
+    
+    private init() { }
+    
+    // MARK: - Functions
+    
+    
+}
